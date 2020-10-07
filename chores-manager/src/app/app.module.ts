6924038ede47839a71974afc21935a9c9b66f5e5
David Maricle <davidnmaricle@gmail.com>
2020-10-06 20:17:20 -0500

    Starting over with a new project!

---
+++ chores-manager/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HelloWorld } from './../components/hello-world.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorld
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
